78e6246bb36c0a7ab72817ff6c8283cf701f1958
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2015-12-12 16:44:14 +0100

    remove vagrant

--- vagrant/puppet/modules/webserver/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# Setups a default virtual host that serves a phpinfo page,
-class webserver {
-  file { '/etc/apache2/sites-enabled/example.dev':
-    path    => '/etc/apache2/sites-enabled/example.dev',
-    source  => 'puppet:///modules/webserver/site.conf',
-    notify  => Service['apache2'],
-    require => Package['apache2'],
-  }
-}
